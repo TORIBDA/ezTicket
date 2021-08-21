7385479292e58cdf5bd21531b7f66a82c5da3aee
TORIBDA <david.moses.toribio@oocl.com>
2021-08-21 22:29:38 +0800

    [chore]: setup backend project

---
+++ Backend/src/test/java/com/ezticket/EzticketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ezticket;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
